da0ce89d46ef07e877ff2f6eb2f5480d87b89e75
beaverwrestler <artin.sarkezians@gmail.com>
2017-04-05 16:18:02 +0300

    Completed file input & object creation

--- gr12cs-a/assignment3/rev0/Main.java
+++ gr12cs-a/assignment3/rev0/Main.java
@@ -2,70 +2,58 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    
+  
     private static double rating = 0;
     private static String categ = "";
     private static String name = "";
     private static int rememberThis = 0;
+    private static ArrayList <Movie> film = new ArrayList();
+ 
+            //ask user what they want to search by
+            //ask what they want to seach
+            //display info
+            //keep looping unitl usr inputs exit OR press any key after to return to main menu
     
     public static void main (String [] args) throws FileNotFoundException {
         System.out.println("Processing...");
-        makeObjects();
-
-        //this comment was made in IntelliJ
+        makeObjects();   
         
-        System.out.println(name);
-        System.out.println(categ);
-        System.out.println(rating);
+        Scanner readInp = new Scanner (System.in);
+        
+        char crit = ' ';
+        
+        while (crit != 'r' || crit != 'g' || crit != 'n') {
+            System.out.println("What would you like to search by? (r = rating, g = genre, n = name)");
+            crit = readInp.next((();
+            
+        }
     }
     
     public static void makeObjects () throws FileNotFoundException {
         Scanner fileInput = new Scanner (new File ("input.txt"));
        
-        for (int j = 0; fileInput.hasNextLine(); j++) {
+        while (fileInput.hasNextLine()) {
             String currLine = fileInput.nextLine();
-            currLine.trim();
+            StringTokenizer counter = new StringTokenizer (currLine, "% ");
+            try {
+                rating = Double.parseDouble (counter.nextToken());   
+            }
+            catch (NumberFormatException e) {
+                System.out.println("There seems to be a problem with this line of input, going to next line");
+                break;
+            }
             
-            StringTokenizer counter = new StringTokenizer (currLine);
-            //skip line if no percent sign
-            if (counter.countTokens() >2)
-            {
-                //finds the index of the first % sign of space starting from the left
-                for (int i = 0; i<currLine.length(); i ++) {
-                    if (currLine.charAt(i) == '%') {
-
-                        rememberThis = i;
-                        rating = Double.parseDouble (currLine.substring(0, i));
-                        currLine = currLine.substring(i);
-                        break;
-                    }
-               
-                }
+            while (counter.hasMoreTokens()) {
+                String test = counter.nextToken();
+                if (counter.hasMoreTokens() == false)
+                    categ = test;
+                else 
+                    name += test + " "; 
+            }
             
-                //finds the index going backwards of the first space
-                for (int i = currLine.length()-1; i>-1; i --)
-                    if (currLine.charAt(i) == ' ' ) {
-                        categ = currLine.substring(i).trim();
-                        name = currLine.substring (rememberThis, i).trim();
-                        break;
-                    }
-           }
-            
-           //code for making objects
-           
-           Movie mv1 = new Movie (name, categ, rating); 
-            
-            //ask user what they want to search by
-            //ask what they want to seach
-            //display info
-            //keep looping unitl usr inputs exit OR press any key after to return to main menu
-            
+            film.add(new Movie (name, categ, rating));            
         } 
-    }
-    
-    public static void printFirstObject ()
-    {
         
-        
+        fileInput.close();
     }
 }
