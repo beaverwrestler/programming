ff5cddab1302c2f5c1eb8246e5f7f93a089b91ca
beaverwrestler <artin.sarkezians@gmail.com>
2017-04-21 16:40:48 +0300

    Small changes

--- gr12cs-a/assignment4/Driver.java
+++ gr12cs-a/assignment4/Driver.java
@@ -12,8 +12,9 @@ public class Driver {
     
     public static void main (String[] args) throws IOException {
     	BufferedReader stdIn = new BufferedReader (new InputStreamReader (System.in));
-        importCDs("Millenium.txt");
+        //importCDs("Millenium.txt");
         String contin = "";
+        String cdName = "";
         
         do {        
         	int mainChoice, subChoice;
@@ -29,21 +30,28 @@ public class Driver {
             
             if (mainChoice == 1) {
                 if (subChoice == 1) {        //this is also fine
-                    System.out.println("\nCDs currently in memory: ");
-                    for (int i = 0; i < cds.size(); i ++) 
-                        System.out.println(cds.get(i));
-                    System.out.println();
+                    if (cds.size() == 0)
+                        System.out.println("There are currently no CDs loaded...");
+                    else {
+                        System.out.println("\nCDs currently in memory: ");
+                        for (int i = 0; i < cds.size(); i ++) 
+                            System.out.println(cds.get(i));
+                    }  
+                }
+                else if (subChoice == 2) { 
+                    cdName =  findCD(stdIn);
+                    //put code to print info on CD here
+                    cdName = "";
                 }
                 else if (subChoice == 3) {        //this is fine
                     while (true) {
-                        System.out.println("Please enter the name of the file/CD you'd like to load into memory (exit to cancel): ");
+                        System.out.println("Please enter the name of the CD you'd like to load into memory (exit to cancel): ");
                         String name = stdIn.readLine();
-                        if (name.equals("exit"))
+                        if (name.equalsIgnoreCase("exit"))
                             break;
                         importCDs(name);    
                     }   
                 }
-                else if (subChoice == 2) {    }
                 else if (subChoice == 4) {    }
                 else if (subChoice == 5) {    }
                 else if (subChoice == 6) {    }
@@ -52,20 +60,16 @@ public class Driver {
             }
         
             else if (mainChoice == 2) {
-               String cdName = "";
-               while ((cdName.equals("") || cdName == null) && !cdName.equals("exit")) {
-                   System.out.println("Please enter the name of the CD you'd like to access (exit to cancel): ");
-                   cdName = stdIn.readLine(); 
-               }
-               if (subChoice ==1) {
+               cdName = findCD(stdIn);
+               if (subChoice ==1) {        //this is fine
                    int found = Collections.binarySearch(cds, new CD (cdName, 0), new compareCD());
-                   if (found < 0 && !cdName.equals("exit")) {
+                   if (found < 0 && !cdName.equalsIgnoreCase("exit")) {
                        System.out.println("That file was not found, was there a typo?");
                        cdName = "";
                    }
-                   else if (!cdName.equals("exit")) {
+                   else if (!cdName.equalsIgnoreCase("exit"))
                        cds.get(found).listSongs();   
-                   }
+                   cdName = "";
                }
                else if (subChoice == 2) {    }
                else if (subChoice == 3) {    }
@@ -75,7 +79,7 @@ public class Driver {
             }
                       
             while (!contin.equals("y") && !contin.equals("yes") && !contin.equals("n") && !contin.equals("no")) {
-                System.out.println ("Would you like to continue? (y/n)");
+                System.out.print ("\nWould you like to continue? (y/n): ");
                 contin = stdIn.readLine().toLowerCase();
             }
             
@@ -114,7 +118,7 @@ public class Driver {
     	    System.out.println ("6) Return back to main menu");
     	    System.out.println ("----------------------------------");
     	}
-	    System.out.print ("\n\nPlease enter your choice:  ");
+	    System.out.print ("\nPlease enter your choice:  ");
         
 	    try {
             int choice = Integer.parseInt (stdIn.readLine ());
@@ -137,6 +141,8 @@ public class Driver {
     }
     
     public static void importCDs (String fileName) {
+        if (fileName.indexOf (".txt") < 0)
+            fileName += ".txt";
         try {
             BufferedReader inp = new BufferedReader (new FileReader (fileName));
             String cdName = inp.readLine();
@@ -152,14 +158,23 @@ public class Driver {
                 String length = inp.readLine();
                 cd.addSong(title, name, genre, rating, length);
             }
-            
             Collections.sort(cds, new compareCD());
+            inp.close();
         }
         catch (FileNotFoundException e) {
             System.out.println("That file was not found, was there a typo?");
         }
         catch (Exception e) {
-            System.out.println("Something bad went wrong, weally bad...");
+            System.out.println("Something went bad, weally bad...");
         }
+    }
+    
+    public static String findCD (BufferedReader stdIn) throws IOException {
+        String cdName = "";
+        while ((cdName.equals("") || cdName == null) && !cdName.equals("exit")) {
+            System.out.println("Please enter the name of the CD you'd like to access (exit to cancel): ");
+            cdName = stdIn.readLine(); 
+        }
+        return cdName;
     }
 }
