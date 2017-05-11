6540e8a130c89e252abd42bbc1e135dc3379e9dd
beaverwrestler <artin.sarkezians@gmail.com>
2017-05-11 16:21:07 +0300

    io for dd

--- gr12cs-a/dynamic-data/dynamic_data_problem1_rev0.java
+++ gr12cs-a/dynamic-data/dynamic_data_problem1_rev0.java
@@ -5,16 +5,18 @@ public class dynamic_data_problem1_rev0 {
     public static void main (String [] args) throws IOException, FileNotFoundException {
         BufferedReader textInp = new BufferedReader (new FileReader ("input.txt"));
         int numLoop = Integer.parseInt (textInp.readLine());
+        System.out.println("Finding the number of Substrings");
         
         for (int k = 0; k < numLoop; k++) {
             Set <String> mySet = new HashSet <String> ();
             String str = textInp.readLine();
+            System.out.println("String: " + str);
             for (int i = 0; i <= str.length(); i++) {
                 for (int j = i; j <= str.length(); j++) {
                     mySet.add(str.substring (i, j));
                 }  
             }
-            System.out.println(mySet.size());
+            System.out.println("No. of Substrings: " + mySet.size());
         }
     }
 }
