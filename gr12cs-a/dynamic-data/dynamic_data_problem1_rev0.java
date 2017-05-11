f5f3ce7dea611e2a229526653cb17cafd2ecca9f
beaverwrestler <artin.sarkezians@gmail.com>
2017-05-11 15:46:13 +0300

    worked on problem 1 of dd

--- gr12cs-a/dynamic-data/dynamic_data_problem1_rev0.java
+++ gr12cs-a/dynamic-data/dynamic_data_problem1_rev0.java
@@ -5,11 +5,20 @@ public class dynamic_data_problem1_rev0 {
     public static void main (String [] args) throws IOException, FileNotFoundException {
         BufferedReader textInp = new BufferedReader (new FileReader ("imput.txt"));
         int numLoop = Integer.parseInt (textInp.readLine());
+        Set <String> mySet = new HashSet <String> ();
         
         for (int i = 0; i < numloop; i++) {
+                    Set <String> mySet = new HashSet <String> ();
+
             String str = textInp.readLine();
-            
+            for (int i = 0; i < str.readLine(); i++) {
+                for (int j = 0; j < str.readLine(); j++) {
+                    mySet.add(str.substring (i, j));
+                }  
+            }
+                    System.out.println(mySet);
             
         }
+        
     }
 }
