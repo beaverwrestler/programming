2d9af9f704d4d9841898b52874783ad2bf62c206
beaverwrestler <artin.sarkezians@gmail.com>
2017-05-18 19:55:34 +0300

    slight changes

--- gr12cs-a/assignment5/Assignment5_rev1.java
+++ gr12cs-a/assignment5/Assignment5_rev1.java
@@ -2,28 +2,26 @@ import java.util.*;
 import java.io.*;
 
 //programs assumes numbers are also words, lower case is same as uppercase
-//fully functional
-
 //BOTH VERSIONS STILL CONSIDER A ' AS A WORD, FIX THIS ----> in ALICE 
 
 public class Assignment5_rev1 {
     public static void main (String [] args) throws FileNotFoundException, IOException {
-        long startTime = System.currentTimeMillis();
-        String FILENAME = "input.txt";
         System.out.println("Most Frequent Words in a File");
-        System.out.println("File name: " + FILENAME);
-        BufferedReader text = new BufferedReader (new FileReader (FILENAME));
+        String fileName = getFileName ();        
+        long startTime = System.currentTimeMillis();
+        System.out.println("File name: " + fileName + "\nBeginning search...");
+        BufferedReader text = new BufferedReader (new FileReader (fileName));
         StringBuilder book = new StringBuilder ();
         String line = text.readLine();
 
-        while (line != null){   //imports the whole goddamn book
+        while (line != null){   //imports the whole book
             book.append(line + " ");
             line = text.readLine();
         }
 
         text.close();
         String finalStr = book.toString().toLowerCase();
-        StringTokenizer words = new StringTokenizer (finalStr, "~!@#$%^&*()_+{}|:\"<>?`=[];, ./-\\");
+        StringTokenizer words = new StringTokenizer (finalStr, "~!@#$%^&*()_+{}|:\"<'>?`=[];, ./-\\");
         HashMap <String, Word> bookMap = new HashMap <> ();
 
         while (words.hasMoreTokens()) {
@@ -51,12 +49,32 @@ public class Assignment5_rev1 {
 
         System.out.println ("Total Time: " + (System.currentTimeMillis() - startTime) + "ms");
         System.out.println("20 Most Frequency Words");
-
-        for (int i = 1; i <21; i++) {
-            System.out.println(i + ") \"" + wordArray.get(i-1).getWord() 
-                               + "\" has " + wordArray.get(i-1).getInsta() + " instances");
-        }
-
+        for (int i = 1; i <21; i++)
+            System.out.println(i + ") \"" + wordArray.get(i-1).getWord() + "\" has " + wordArray.get(i-1).getInsta() + " instances");
         System.out.println("Program is Complete");
+    }
+    
+    private static String getFileName () {
+        BufferedReader usrIO = new BufferedReader (new InputStreamReader (System.in));
+        String name = "";
+        
+        while (name.isEmpty()) {
+            try {
+                System.out.print("What is the name of the file you'd like to open?: ");
+                name = usrIO.readLine();
+                if (!name.contains (".txt"))    //if user forgets to add ".txt"
+                    name += ".txt";
+                BufferedReader testing = new BufferedReader (new FileReader (name));
+                testing.close();
+                usrIO.close();
+            }
+            catch (FileNotFoundException e) {
+                name = "";
+            }
+            catch (IOException e) {
+                name = "";
+            }
+        }
+        return name;
     }
 }
